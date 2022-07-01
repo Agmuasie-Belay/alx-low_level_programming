bdc9e36345f3d98be05b95cd8d33947c089e5cb5
Agmuasie-Belay <agmuasie.b@gmail.com>
2022-06-30 22:30:56 -0700

    This is my first trial

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+/**
+ * This is my first program in the shell
+ *
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
