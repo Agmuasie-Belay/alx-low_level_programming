04467b18263a079bc39ce8dcfc2beb6108615be6
Agmuasie-Belay <106754601+Agmuasie-Belay@users.noreply.github.com>
2022-07-03 14:51:07 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
