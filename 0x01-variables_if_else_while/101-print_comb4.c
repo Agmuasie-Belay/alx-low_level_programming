dbd9045b5f2a32172d87b229da04c96b234ef89a
Agmuasie-Belay <106754601+Agmuasie-Belay@users.noreply.github.com>
2022-07-03 15:30:09 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+
+	for (i = 48; i <= 57; i++)
+	for (j = i; j <= 57; j++)
+	{
+	if (i == j)
+		continue;
+	putchar(i);
+	putchar(j);
+	if (i == 56 && j == 57)
+		break;
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
