79b138bd9ec7d9dd16cc89babbd9a0a8a84e8e9c
Agmuasie-Belay <106754601+Agmuasie-Belay@users.noreply.github.com>
2022-07-03 15:04:34 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+	putchar(c);
+	if (c == 57)
+		break;
+	putchar(',');
+	putchar(' ');
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
