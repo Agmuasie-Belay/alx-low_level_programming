dad36de8c70a52c2c1132b1e21fd8a5174c80322
Agmuasie-Belay <106754601+Agmuasie-Belay@users.noreply.github.com>
2022-07-03 14:33:55 +0300

    Update aac.c

--- 0x01-variables_if_else_while/aac.c
+++ 0x01-variables_if_else_while/aac.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+	printf("%d", c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
 
