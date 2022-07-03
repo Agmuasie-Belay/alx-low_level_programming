d5d5756b456186279ef51ed8f702dd9f9fe00f1f
Agmuasie-Belay <106754601+Agmuasie-Belay@users.noreply.github.com>
2022-07-03 16:06:02 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+	int k;
+
+	for (i = 48; i <= 57; i++)
+	for (j = i; j <= 57; j++)
+	{
+	if (i == j)
+		continue;
+	for (k = j; k <= 57; k++)
+	{
+		if (j == k)
+			continue;
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		if (i == 55 && j == 56 && k == 57)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
