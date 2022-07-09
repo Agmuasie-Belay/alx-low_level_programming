ffee530e66476c5d4f38153e6ba5d9bae8985b47
Agmuasie-Belay <106754601+Agmuasie-Belay@users.noreply.github.com>
2022-07-09 10:54:16 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Test function for positive or negative
- *
  * Return: Always 0 (Success)
  */
 
@@ -12,5 +11,6 @@ int main(void)
         
         i = 0;
         positive_or_negative(i);
+        
         return (0);
 }
